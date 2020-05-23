ee5f984bad3a44e11ea271a198b28a573180be99
Sergey Shkret <freedoms170691@gmail.com>
2020-05-23 19:47:22 +0300

    commit

--- script.js
+++ script.js
@@ -8,5 +8,12 @@ menu.onclick = function myFunction() {
 	}
 }
 
+function slowScroll(id) {
+	var offset = 0;
+	$('html, body').animate({
+		scrollTop: $(id).offset().top - offset
+	}, 1000);
+	return false;
 
+}
 
